f6bef244a4acaab9dd4ade08afbbe88492e502ff
unknown <arefin.khan8364@gmail.com>
2023-03-16 12:09:43 +0600

    server update

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ function verifyJWT(req, res, next) {
 
 }
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hkb9l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hkb9l.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
